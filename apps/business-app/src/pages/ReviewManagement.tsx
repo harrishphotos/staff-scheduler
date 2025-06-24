4ee7e8752ed92e83aaa54a9776ec9edafc5ca0ad
Kanchan1015 <akanchan0315@gmail.com>
2025-06-24 13:24:09 +0530

    chore: clean up unused files and components

--- apps/business-app/src/pages/ReviewManagement.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FeedbackManagement: React.FC = () => {
-  return <div>Feedback Management</div>;
-};
-
-export default FeedbackManagement;
--- apps/business-app/src/pages/ServiceManagement.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ServiceManagement: React.FC = () => {
-  return <div>Service Management</div>;
-};
-
-export default ServiceManagement;
